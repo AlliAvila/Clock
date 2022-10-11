b6fdf81ec8d3698b3282c51b3cfd65fb5c40ba2d
unknown <Allison@MSI-A01338.mps.k12>
2022-10-11 12:40:31 -0700

    DD

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ function updateClock(){
     const minute = currentDate.getMinutes()
     const second= currentDate.getSeconds()
     const hourDeg = (hour / 12) * 360;
-    hourE1.style.transform = 'rotate(${hourDeg}deg)';
+    hourE1.style.transform = `rotate(${hourDeg}deg)`
     const minuteDeg = (minute / 60) * 360;
-    minuteE1.style.transform = 'rotate(${minuteDeg}deg)';
+    minuteE1.style.transform = `rotate(${minuteDeg}deg)`;
     const secondDeg = (second / 60) * 360;
-    secondE1.style.transform = 'rotate(${secondDeg}deg)';
+    secondE1.style.transform = `rotate(${secondDeg}deg)`;
 }
 
-updateClock();
+//updateClock();
+
+setInterval(updateClock, 1000)
